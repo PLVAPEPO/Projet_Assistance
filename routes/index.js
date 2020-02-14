23495b73d3c14407ed4d5075b311edf0d7185e18
kirtsu04 <lucas.ru74@gmail.com>
2020-02-14 11:43:03 +0100

    yes

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,33 @@
-let con = require('../db');
+//let con = require('../db');
 var express = require('express');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/connexion', (req, res) => {
+  console.log('Inside GET /login callback function')
+  console.log(req.sessionID)
+  res.send(`You got the login page!\n`)
+})
+
+router.post('/connexion', (req, res) => {
+  console.log('Inside POST /login callback')
+  passport.authenticate('local', (err, user, info) => {
+    console.log('Inside passport.authenticate() callback');
+    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+    console.log(`req.user: ${JSON.stringify(req.user)}`)
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback')
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`)
+      return res.send('You were authenticated & logged in!\n');
+    })
+  })(req, res, next);
+})
+
 module.exports = router;
-
-
-
-var login = function(req, res) {
-  req.session.connected=true;
-}
-
-var checkIfLoggedIn = function(req, res, next) {
-  if(req.session.loggedIn)
-    next();
-  else
-    res.redirect("/index");
-}
-
-var logout = function(req, res){
-  req.session.destroy(function(err) {
-    if(err) console.log(err);
-    res.redirect('/');
-  })
-}
