9685f76ec5a33995463c9274338c8958b0469dbe
kirtsu04 <lucas.ru74@gmail.com>
2020-02-14 10:38:40 +0100

    j

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,23 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+var login = function(req, res) {
+  req.session.connected=true;
+}
+
+var checkIfLoggedIn = function(req, res, next) {
+  if(req.session.loggedIn)
+    next();
+  else
+    res.redirect("/index");
+}
+
+var logout = function(req, res){
+  req.session.destroy(function(err) {
+    if(err) console.log(err);
+    res.redirect('/');
+  })
+}
