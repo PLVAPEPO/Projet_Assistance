70097d90389742e2c348210da29a0a6d7a11460d
BenoitPE <benoitpegaz@gmail.com>
2020-02-18 15:58:15 +0100

    2020/02/18 16:00 Ben

--- routes/ajouterBillet.js
+++ routes/ajouterBillet.js
@@ -3,17 +3,32 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res) {
-	res.render('ajouterBillet')
+router.get('/', function (req, res) {
+	let query = 'SELECT * FROM ORDINATEUR';
+	con.query(query, (err, rows) => {
+		if (err) throw err;
+		res.render('ajouterBillet', { 'ordis': rows });
+	});
+
 });
 
-router.get("/confirm",function(req, res) {
+router.get("/confirm", function (req, res) {
 	console.log(req.query);
-	con.query('INSERT INTO BILLET SET ?', { 'TITREBILLET': req.query.title, 'URGENCEBILLET': req.query.priority},
-	 (err,rows) => {
-		if(err) throw err;
-		res.redirect('/tickets');
-	  });
+	con.query('INSERT INTO BILLET SET ?', { 'TITREBILLET': req.query.title, 'URGENCEBILLET': req.query.priority },
+		(err, rows) => {
+			if (err) throw err;
+			res.redirect('/tickets');
+		});
+});
+
+router.post("/", function (req, res) {
+	console.log(req.query);
+	con.query('INSERT INTO BILLET SET ?', { 'TITREBILLET': req.query.title, 'URGENCEBILLET': req.query.priority },
+		(err, rows) => {
+			if (err) throw err;
+			// res.redirect('/tickets');
+			res.json(rows);
+		});
 });
 
 module.exports = router;
