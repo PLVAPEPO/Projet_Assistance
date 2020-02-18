981f14c63183a0cfc964343e70858364f077e676
BenoitPE <benoitpegaz@gmail.com>
2020-02-18 13:49:17 +0100

    2020/02/18 13:50 Ben

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var billetsRouter = require('./routes/billets');
 var billetRouter = require('./routes/billet');
-var addTicketRouter = require('./routes/addticket');
+var ajouterBilletRouter = require('./routes/ajouterBillet');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -64,7 +64,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/billets', billetsRouter);
 app.use('/billet', billetRouter);
-app.use('/addticket', addTicketRouter);
+app.use('/ajouterBillet', ajouterBilletRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/ajouterBillet.js
+++ routes/ajouterBillet.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res) {
-	res.render('addticket')
+	res.render('ajouterBillet')
 });
 
 router.get("/confirm",function(req, res) {
