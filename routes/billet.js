659d4baaf9d1d6e7690c9004838f678fd5e477e6
BenoitPE <benoitpegaz@gmail.com>
2020-02-18 10:07:31 +0100

    2020/02/18 10:07 Ben

--- routes/billet.js
+++ routes/billet.js
@@ -3,12 +3,12 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/:id', function (req, res) {
+router.get('/:id', function (req, res, next) {
     let query = 'SELECT * FROM BILLET WHERE IDBILLET = ?';
     con.query(query, req.params.id, (err, rows) => {
         if (err) throw err;
-        //res.render('billet', { 'billet': rows });
-        res.json(rows);
+        res.render('billet', { 'billet': rows });
+        // res.json(rows);
     });
 });
 
