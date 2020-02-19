e111042b013330cb88ec01b1ec16b085ccf287b4
BenoitPE <benoitpegaz@gmail.com>
2020-02-19 11:33:20 +0100

    2020/02/19 11:33 Ben

--- routes/ajouterBillet.js
+++ routes/ajouterBillet.js
@@ -7,8 +7,13 @@ router.get('/', function (req, res) {
 	let query = 'SELECT * FROM ORDINATEUR';
 	con.query(query, (err, rows) => {
 		if (err) throw err;
-		res.render('ajouterBillet', { 'ordis': rows });
+		let query2 = 'SELECT * FROM PROBLEME';
+		con.query(query2, (err, rows2) => {
+			if (err) throw err;
+			res.render('ajouterBillet', { 'ordis': rows, 'probs': rows2 });
+		});
 	});
+
 
 });
 
@@ -22,12 +27,25 @@ router.get("/confirm", function (req, res) {
 });
 
 router.post("/", function (req, res) {
-	console.log(req.query);
-	con.query('INSERT INTO BILLET SET ?', { 'TITREBILLET': req.query.title, 'URGENCEBILLET': req.query.priority },
+	var d = new Date();
+	let dateY = d.getUTCFullYear()
+	let dateM = d.getUTCMonth()+1
+	let dateD = d.getDate()
+	let dateC = '' + dateY + '-' + dateM + '-' + dateD
+	con.query('INSERT INTO BILLET SET ?',
+		{
+			'TITREBILLET': req.body.titreBillet,
+			'IDPROBLEME': req.body.problemeBillet,
+			'URGENCEBILLET': req.body.urgenceBillet,
+			'MESSAGEBILLET': req.body.messageBillet,
+			'IDORDINATEUR': req.body.posteOrdinateur,
+			'ETATBILLET': 0,
+			'DATECREATIONBILLET': dateC
+			},
 		(err, rows) => {
 			if (err) throw err;
-			// res.redirect('/tickets');
-			res.json(rows);
+			res.redirect('/');
+			// res.json(req.body.titreBillet);
 		});
 });
 
--- routes/billets.js
+++ routes/billets.js
@@ -1,11 +1,17 @@
 let con = require('../db');
 var express = require('express');
 var router = express.Router();
-
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
 
 router.get('/', function (req, res, next) {
-  let query = 'SELECT * FROM BILLET';
-  con.query(query, (err, rows) => {
+  var d = new Date();
+  let date = '' + d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1, 2) + "-%"
+  let query = 'SELECT * FROM BILLET WHERE DATECREATIONBILLET LIKE ?';
+  con.query(query, date + "%", (err, rows) => {
     if (err) throw err;
     res.render('billets', { 'billets': rows });
   });
