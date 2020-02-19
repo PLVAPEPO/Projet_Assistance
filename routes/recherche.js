b0ed43236d331ef1642afe8b026f74113a5955f5
BenoitPE <benoitpegaz@gmail.com>
2020-02-19 16:50:18 +0100

    2020/02/19 16:50 Ben

--- routes/recherche.js
+++ routes/recherche.js
@@ -18,46 +18,20 @@ router.get('/', function (req, res) {
 });
 
 router.post("/", function (req, res) {
-
-	// res.json(query + recherche)
 	if (req.body.typeRechercheBillet == "") {
 		res.redirect('/recherche')
 	}
 	else {
-		if (req.body.typeRechercheBillet == "titreBillet") {
-			let query = "SELECT * FROM BILLET WHERE TITREBILLET like ?";
+		// if (req.body.typeRechercheBillet == "titreBillet" || req.body.typeRechercheBillet == "") {
+			let type = '' +  req.body.typeRechercheBillet + ''
+			let query = "SELECT * FROM BILLET WHERE "+type+" like ?";
 			let recherche = '%' + req.body.rechercheBillet + '%'
 			con.query(query, recherche, (err, rows) => {
 				if (err) throw err;
 				res.render('recherche', { 'billets': rows });
 			});
-		}
+		// }
 	}
 });
-
-/*router.post("/", function (req, res) {
-		var d = new Date();
-		let dateY = d.getUTCFullYear()
-		let dateM = d.getUTCMonth() + 1
-		let dateD = d.getDate()
-		let dateC = '' + dateY + '-' + dateM + '-' + dateD
-		con.query('INSERT INTO BILLET SET ?',
-			{
-				'TITREBILLET': req.body.titreBillet,
-				'IDPROBLEME': req.body.problemeBillet,
-				'URGENCEBILLET': req.body.urgenceBillet,
-				'MESSAGEBILLET': req.body.messageBillet,
-				'IDORDINATEUR': req.body.posteOrdinateur,
-				'ETATBILLET': 0,
-				'DATECREATIONBILLET': dateC
-			},
-			(err, rows) => {
-				if (err) throw err;
-				res.redirect('/');
-				// res.json(req.body.titreBillet);
-			});
-
-});
- */
 
 module.exports = router;
