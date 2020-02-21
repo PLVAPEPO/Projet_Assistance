db7e0b2908f090bebbee9ea352333789f0c7f60c
kirtsu04 <lucas.ru74@gmail.com>
2020-02-21 15:07:59 +0100

    2020/02/21 Plume

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,20 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/connexion', (req, res) => {
-  console.log('Inside GET /login callback function')
-  console.log(req.sessionID)
-  res.send(`You got the login page!\n`)
-})
-
-router.post('/connexion', (req, res) => {
-  console.log('Inside POST /login callback')
-  passport.authenticate('local', (err, user, info) => {
-    console.log('Inside passport.authenticate() callback');
-    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-    console.log(`req.user: ${JSON.stringify(req.user)}`)
-    req.login(user, (err) => {
-      console.log('Inside req.login() callback')
-      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-      console.log(`req.user: ${JSON.stringify(req.user)}`)
-      return res.send('You were authenticated & logged in!\n');
-    })
-  })(req, res, next);
-})
+console.log('Yes')
 
 module.exports = router;
+
+router.post('/billets',function(req, res, next) {
+  function Connexion()
+  {
+    if(uname="" && psw =="")
+    {
+      res.redirect('index',{title: 'Express'});
+    }
+    else
+    {
+      res.redirect('/billets',{title:'Billets'});
+    }
+  }
+});
