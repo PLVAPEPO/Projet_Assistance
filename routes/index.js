481f1d701858eb56b2e0d7c08d99b4d1ed2f3144
BenoitPE <benoitpegaz@gmail.com>
2020-03-06 08:18:08 +0100

    2020-02-06 08:17

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
-//let con = require('../db');
+let con = require('../db');
 var express = require('express');
 var router = express.Router();
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  let query = 'SELECT * FROM PERSONNE';
+  con.query(query, (err, rows) => {
+    if (err) throw err;
+    res.render('index', { 'personnes': rows });
+  });
+});
+
+router.post("/", function (req, res) {
+  con.query('SELECT * FROM PERSONNE',
+    (err, rows) => {
+      if (err) throw err;
+      res.json(req.body.titreBillet);
+    });
 });
 
 module.exports = router;
