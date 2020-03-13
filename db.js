09eb1aa77946a811a2f49bbc70466a68ff81c240
kirtsu04 <lucas.ru74@gmail.com>
2020-03-13 10:47:44 +0100

    2020-03-13 10:41 plume

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 let mysql = require('mysql');
 let con = mysql.createConnection({
-<<<<<<< HEAD
     host: 'vps409067.ovh.net',
     port: '3306',
     user: 'grp1',
@@ -8,16 +7,6 @@ let con = mysql.createConnection({
     database: 'grp1',
     multipleStatements: true
   });
-=======
-  host: 'vps409067.ovh.net',
-  port: '3306',
-  user: 'grp1',
-  password: 'UivAc8mO5vZrIF0w',
-  database: 'grp1',
-  multipleStatements: true
-});
-
->>>>>>> 99d0097e77f0d8cea418bbbf1491e0cca9e251b1
   
   con.connect((err) => {
     if(err){
