cb4f9bbfc8d5d15a0a6fcef616af0e5d47b90bce
kirtsu04 <lucas.ru74@gmail.com>
2020-03-13 15:26:05 +0100

    2020-03-13 13:27 plume

--- routes/billet.js
+++ routes/billet.js
@@ -1,15 +1,24 @@
 let con = require('../db');
 var express = require('express');
 var router = express.Router();
-
+var idbill;
 
 router.get('/:id', function (req, res, next) {
-    
+    idbill = req.params.id;
     let query = 'SELECT * FROM BILLET JOIN PROBLEME ON BILLET.IDPROBLEME = PROBLEME.IDPROBLEME WHERE BILLET.IDBILLET = ?';
     con.query(query, req.params.id, (err, rows) => {
-        if (err) throw err;
+        if (err) throw err;       
         res.render('billet', { 'billet': rows, pseudo: req.session.pseudo});
     });
+});
+
+router.post("/", function (req, res) {
+    con.query("Update BILLET set ETATBILLET ='1' where idbillet = ?",idbill,(err, rows) => {
+            if (err) throw err;
+            res.redirect('/billets');
+            // res.json(req.body.titreBillet);
+        });
+
 });
 
 module.exports = router;
