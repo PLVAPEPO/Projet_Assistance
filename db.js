b672df6b0a177fe2c89b3a78524ea361e961b1f2
BenoitPE <benoitpegaz@gmail.com>
2020-03-13 11:08:14 +0100

    2020-03-13 11:07 Ben

--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 let mysql = require('mysql');
 let con = mysql.createConnection({
-<<<<<<< HEAD
-    host: 'vps409067.ovh.net',
-    port: '3306',
-    user: 'grp1',
-    password: 'UivAc8mO5vZrIF0w',
-    database: 'grp1',
-    multipleStatements: true
-  });
-=======
   host: 'vps409067.ovh.net',
   port: '3306',
   user: 'grp1',
@@ -17,7 +8,6 @@ let con = mysql.createConnection({
   multipleStatements: true
 });
 
->>>>>>> 99d0097e77f0d8cea418bbbf1491e0cca9e251b1
   
   con.connect((err) => {
     if(err){
