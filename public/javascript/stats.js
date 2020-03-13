b3e59502ca29c08df8b5051de2db1b034aa65b43
BenoitPE <benoitpegaz@gmail.com>
2020-03-13 15:50:01 +0100

    2020-03-13 15:47 Ben

--- public/javascript/stats.js
+++ public/javascript/stats.js
@@ -6,12 +6,4 @@ if(typeof document.querySelector('#idmois') !== 'undefined') {
     document.getElementById(str).classList.add('active');
 }
 
-/*months.forEach((onemonth) =>{
-    onemonth.addEventListener('click',function(){
-        let active = document.querySelector('.active');
-        active.classList.toggle('active');
-        onemonth.querySelector('a').classList.toggle('active');
-    })
-})*/
-
 
