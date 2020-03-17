895889fa0dc13ca0f5c8ab510378b65c73913f33
kirtsu04 <lucas.ru74@gmail.com>
2020-03-17 18:25:22 +0100

    ca m fait chier

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ var login = function (req, res, next) {
       let cipher = crypto.createCipheriv(algorithm, key, iv);
       let encrypted = cipher.update(req.body.psw, 'utf8', 'hex');
       encrypted += cipher.final('hex');
+      console.log(encrypted)
       if(rows.length === 1 && rows[0].MDPPersonne === encrypted)
       {
         req.session.connected=true;
@@ -111,7 +112,7 @@ app.use('/login', login, billetsRouter);
 app.use('/logout', logout, indexRouter);
 app.use('/billets', checkLoggedIn, billetsRouter);
 // app.use('/billets',checkLoggedIn, billetsRouter);
-app.use('/billet', checkLoggedIn, billetRouter);
+app.use('/billet/', checkLoggedIn, billetRouter);
 app.use('/recherche', rechercheRouter);
 app.use('/stats', statsRouter);
 app.use('/ajouterBillet', checkLoggedIn, ajouterBilletRouter);
