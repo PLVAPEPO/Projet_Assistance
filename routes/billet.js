b1ff5679732be30b3f230438f7c503e7391b1f04
BenoitPE <benoitpegaz@gmail.com>
2020-03-17 18:30:25 +0100

    2020-03-17 18:30 Ben

--- routes/billet.js
+++ routes/billet.js
@@ -6,9 +6,14 @@ var idbill;
 router.get('/:id', function (req, res, next) {
     idbill = req.params.id;
     let query = 'SELECT * FROM BILLET JOIN PROBLEME ON BILLET.IDPROBLEME = PROBLEME.IDPROBLEME WHERE BILLET.IDBILLET = ?';
+    let query2 = 'SELECT * FROM COMMENTAIRE C JOIN BILLET B ON C.IDBILLET = B.IDBILLET WHERE B.IDBILLET = ? ORDER BY C.IDCOMMENTAIRE DESC'
     con.query(query, req.params.id, (err, rows) => {
-        if (err) throw err;       
-        res.render('billet', { 'billet': rows, pseudo: req.session.pseudo, role : req.session.role, prenom : req.session.prenom, nom: req.session.nom});
+        if (err) throw err;
+        con.query(query2, req.params.id, (err2, rows2) => {
+            if (err2) throw err2;
+            // res.json(rows2)
+            res.render('billet', { 'billet': rows, 'comms': rows2, pseudo: req.session.pseudo, role: req.session.role, prenom: req.session.prenom, nom: req.session.nom, idpers: req.session.idPersonne });
+        });
     });
 });
 
@@ -24,7 +29,7 @@ router.post("/", function (req, res) {
             idpers = rows[0].IDPERSONNE
             var d = new Date();
             let dateY = d.getUTCFullYear()
-            let dateM = d.getUTCMonth() + 2
+            let dateM = d.getUTCMonth() + 1
             let dateD = d.getDate()
             let dateC = '' + dateY + '-' + dateM + '-' + dateD
             con.query('select max(IDINTERVENTION) as maxid from INTERVENTION', (err, rows) => {
@@ -64,6 +69,23 @@ router.post("/", function (req, res) {
 
             res.redirect('/billets');
     }
+});
+
+router.post("/ajout", function (req, res) {
+    var d2 = new Date();
+    let dateY2 = d2.getUTCFullYear()
+    let dateM2 = d2.getUTCMonth() + 1
+    let dateD2 = d2.getDate()
+    if (dateM2 < 10) {
+        dateM2 = '0' + dateM2;
+    }
+    let dateC2 = '' + dateY2 + '-' + dateM2 + '-' + dateD2
+    // res.json(req.body)
+    let queryAjout = "INSERT INTO COMMENTAIRE(IDBILLET, TITRECOMMENTAIRE, LIBELLECOMMENTAIRE, DATECOMMENTAIRE) VALUES ('" + req.body.idbilletajout + "','" + req.body.titrecommentaire + "','" + req.body.libellecommentaire + "','" + dateC2 + "') "
+    con.query(queryAjout, (err, rows) => {
+        if (err) throw err;
+        res.redirect('/billet/' + req.body.idbilletajout)
+    })
 });
 
 router.post("/", function (req, res) {
